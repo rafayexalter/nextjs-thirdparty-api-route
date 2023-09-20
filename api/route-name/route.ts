2444c8485cf7a015eaea4c5d74818a66d2f53e53
Abdul Rafay <rafayexalter@gmail.com>
2023-09-20 17:09:14 +0200

    Added readme

--- api/route-name/route.ts
+++ api/route-name/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   // Fetch data from third party domain in Next.js route
-  const res = await fetch(process.env.API);
+  const res = await fetch(process.env.FAST_API!);
   const data = await res.json();
 
   return NextResponse.json(data);
